904a36c90d5978650ab7a1daa6156b62ce2ed3d6
Rivers Marc <m.t.rivers@hotmail.com>
2021-02-26 13:46:42 -0600

    major refactor of home page

--- project3-app/src/Components/Home/Home.js
+++ project3-app/src/Components/Home/Home.js
@@ -1,60 +1,49 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import Results from '../Results/Results';
+import { Link, Redirect } from 'react-router-dom';
+
 
 class Home extends Component {
     constructor(props) {
         super(props)
     
-        this.state = {
-          citySelected: [],
-          stateSelected: []
-        }
       }
-
-    handleCity=(e)=>{ 
-        e.preventDefault();
-        this.setState ({
-            citySelected: e.target.value
-        })
-    }
-    handleState=(e)=>{ 
-        e.preventDefault();
-        this.setState ({
-            stateSelected: e.target.value
-        })
-    }
-    handleCitySubmit=(e)=>{ 
-        e.preventDefault();    
-        this.props.citySearch(this.state.citySelected);
-    }
-    handleStateSubmit=(e)=>{ 
-        e.preventDefault();    
-        this.props.stateSearch(this.state.stateSelected);
-    }
 
     render() {
         console.log(this.props);
-
+        console.log(window.location)
         return (
             <div className="Home">
                 <div>
-                    <input type="text" placeholder="City Name" onChange={this.handleCity}/>
+                <form onSubmit={(event)=>{
+                    event.preventDefault();
+                    this.props.citySearch(event.target.city.value)
+                    this.props.history.push("/results")
+                }}>
+                    <input type="text" id="City Name" name="city"/>
                     
-                    <button type='submit' onClick={this.handleCitySubmit}>
-                    <Link to='/results'>Submit</Link>
-                    </button>
+                    <input type='submit' value="submit"/>
+                    
                     <br /><br />
+                    </form><br />
                 </div>
                 
                 <div>
-                    <input type="text" placeholder="State Name" onChange={this.handleState}/>
+                <form onSubmit={(event)=>{
+                    event.preventDefault();
+                    this.props.stateSearch(event.target.state.value)
+                    this.props.history.push("/results")
                     
-                    <button type='submit' onClick={this.handleStateSubmit}>
-                        <Link to='/results'>Submit</Link>
-                    </button>
+                }}>
+                    <input type="text" id="State Name" name="state"/>
                     
+                    <input type='submit' value="submit"/>
+                    
+                    <br /><br />
+                    </form><br />
                 </div>
+
+                
+
             </div>
         );
     }
